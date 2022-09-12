e046aef6a4d06a945cd552e5040fab50224e31a0
andikahadisaputra <andikahadisaputra@gmail.com>
2022-09-12 11:10:41 +0800

    initial commit

---
+++ script/8dir.js
@@ -0,0 +1,88 @@
+"use strict";
+// const arrWinX = [
+//   [0, 0, 0, 0, 0, 0],
+//   [0, 0, 0, 0, 0, 0],
+//   [1, 1, 1, 1, 1, 0],
+//   [0, 0, 0, 0, 0, 0],
+//   [0, 0, 0, 0, 0, 0],
+//   [0, 0, 0, 0, 0, 0]
+// ];
+
+// const arrWinX = [
+//   [0, 0, 0, 0, 0, 0],
+//   [0, 1, 1, 1, 1, 1],
+//   [0, 0, 0, 0, 0, 0],
+//   [0, 0, 0, 0, 0, 0],
+//   [0, 0, 0, 0, 0, 0],
+//   [0, 0, 0, 0, 0, 0]
+// ];
+
+// [0, 0, 0, 0, 0, 0, 0, 0, 0],
+// [0, 0, 0, 0, 0, 0, 0, 0, 0],
+// [0, 0, 0, 0, 0, 0, 0, 0, 0],
+// [0, 0, 0, 0, 0, 0, 0, 0, 0],
+// [0, 0, 0, 0, 0, 0, 0, 0, 0],
+// [0, 0, 0, 0, 0, 0, 0, 0, 0],
+// [0, 0, 0, 0, 0, 0, 0, 0, 0],
+// [0, 0, 0, 0, 0, 0, 0, 0, 0],
+// [0, 0, 0, 0, 0, 0, 0, 0, 0],
+
+//   [0, 1, 1, 1, 1, 1],
+//   [0, 0, 0, 0, 0, 0],
+//   [0, 0, 0, 0, 0, 0],
+//   [0, 0, 0, 0, 0, 0],
+//   [0, 0, 0, 0, 0, 0]
+
+const arrWinX = [
+  [1, 0, 0, 0, 0, 0],
+  [0, 1, 0, 0, 0, 0],
+  [0, 0, 1, 0, 0, 0],
+  [0, 0, 0, 1, 0, 0],
+  [0, 0, 0, 0, 1, 0],
+  [0, 0, 0, 0, 0, 0]
+];
+
+
+function checkWinner(xStart, yStart, arrWinX) {
+  const xDir = [0, 1, 1, 1, 0, -1, -1, -1]; // pair of x[i]y[i] represent direction of checking
+  const yDir = [1, 1, 0, -1, -1, -1, 0, 1]; // south, southeast,east,....,southwest
+
+  // let xStart = 1; //this will be an x,y input of stone placed
+  // let yStart = 1;
+  function isWithinLimit(x, y, arr) {
+    if ((x >= 0 && x < arr[0].length) && (y >= 0 && y < arr.length)) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  for (let i = 0; i < 8; i++) {
+    let numCount = 0;
+    let xTracker = xStart;
+    let yTracker = yStart;
+    let stop = false;
+
+
+    for (let j = 0; j < 5 && isWithinLimit(xTracker, yTracker, arrWinX); j++) {
+      console.log(xTracker);
+      console.log(yTracker);
+      if ((arrWinX[yTracker][xTracker]) === 1) {
+        numCount += 1;
+      }
+      xTracker += xDir[i];
+      yTracker += yDir[i];
+      console.log(`Numcount : ${numCount}`);
+      if (numCount >= 5) {
+        stop = true; //exit nested for loop
+        console.log("winner");
+        console.log(stop);
+      }
+    }
+    if (stop === true) {
+      break;
+    }
+  }
+}
+
+checkWinner(4, 4, arrWinX);
---
+++ script/main.js
@@ -0,0 +1,232 @@
+"use strict";
+
+
+const arrWinX = [
+  [0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 1, 2, 0],
+  [1, 1, 1, 1, 2, 0],
+  [0, 0, 0, 1, 2, 0],
+  [0, 0, 0, 1, 2, 0],
+  [0, 0, 0, 1, 2, 0]
+];
+
+// const arrWinX = [
+//   [1, 3, 6, 0, 0, 0],
+//   [2, 5, 0, 0, 0, 0],
+//   [4, 1, 1, 1, 1, 0],
+//   [0, 0, 0, 0, 0, 0],
+//   [0, 0, 0, 0, 0, 0],
+//   [0, 0, 0, 0, 0, 0]
+// ];
+
+// const arrWinX = [
+//   [0, 0, 0, 0, 0, 0],
+//   [0, 0, 0, 0, 0, 0],
+//   [1, 1, 1, 1, 1, 0],
+//   [4, 0, 0, 0, 0, 0],
+//   [2, 5, 0, 0, 0, 0],
+//   [1, 3, 6, 0, 0, 0]
+// ];
+
+
+
+
+//traverse per column
+// const rowColumn = [];
+
+// 
+
+// console.log(rowColumn);
+
+
+function checkWinner(arr) {
+  //check per row if there's 5 in a row
+  function isFive(count) {
+    if (count === 5) {
+      return true
+    }
+  }
+
+  for (let i = 0; i < arr.length; i++) {
+    let consecutiveCount = 0;
+    for (let j = 0; j < arr[0].length; j++) {
+      if ((consecutiveCount === 0 || arr[i][j] === arr[i][j - 1]) && arr[i][j] !== 0) { //if start of a new consecutive, or stone is same as previous
+        consecutiveCount += 1;
+        console.log(consecutiveCount);
+        if (isFive(consecutiveCount)) {
+          return `We have a winner : ${arr[i][j]}`
+        }
+      } else { // if stone is different 
+        consecutiveCount = 0;
+      }
+    }
+  }
+
+  //check per column if there's 5 in a row
+
+  for (let j = 0; j < arr[0].length; j++) {
+    let consecutiveCount = 0
+    for (let i = 0; i < arr.length; i++) {
+      if ((consecutiveCount === 0 || arr[i][j] === arr[i - 1][j]) && arr[i][j] !== 0) { //if start of a new consecutive, or stone is same as previous
+        consecutiveCount += 1;
+        console.log(consecutiveCount);
+        if (isFive(consecutiveCount)) {
+          return `We have a winner : ${arr[i][j]}`
+        }
+      } else { // if stone is different 
+        consecutiveCount = 0;
+      }
+    }
+  }
+};
+
+console.log(checkWinner(arrWinX));
+
+
+function diagonal1() {
+  //traverse diagonal
+  const m = arrWinX.length;
+  const n = arrWinX[0].length;
+  for (let k = 0; k <= m - 1; k++) {
+    let i = k;
+    let j = 0;
+    while (i >= 0) {
+      console.log(arrWinX[i][j])
+      i -= 1;
+      j += 1;
+    }
+  }
+
+  for (let k = 1; k <= n - 1; k++) {
+    let i = n - 1;
+    let j = k;
+    while (j <= n - 1) {
+      console.log(arrWinX[i][j])
+      i -= 1;
+      j += 1;
+    }
+  }
+};
+
+function diagonal2() {
+  const m = arrWinX.length;
+  const n = arrWinX[0].length;
+  for (let k = m - 1; k >= 0; k--) {
+    let i = k;
+    let j = 0;
+    while (i <= m - 1) {
+      console.log(arrWinX[i][j])
+      i += 1;
+      j += 1;
+    }
+  }
+
+  for (let k = 1; k <= n - 1; k++) {
+    let i = 0;
+    let j = k;
+    while (j <= n - 1) {
+      console.log(arrWinX[i][j])
+      i += 1;
+      j += 1;
+    }
+  }
+};
+
+
+// diagonal1();
+
+// diagonal2();
+
+const smallArr1 = [
+  [1, 2, 3],
+  [7, 8, 9],
+  [13, 14, 15]
+];
+
+const smallArr2 = [
+  [4, 5, 6],
+  [10, 11, 12],
+  [16, 17, 18]
+];
+
+const smallArr3 = [
+  [19, 20, 21],
+  [25, 26, 27],
+  [31, 32, 33]
+];
+
+const smallArr4 = [
+  [22, 23, 24],
+  [28, 29, 30],
+  [34, 35, 36]
+];
+
+
+function clockwiseRotate(smallArr) {
+  const arr = smallArr;
+  let n = arr.length;
+
+  for (let i = 0; i < 3; i++) {
+    for (let j = i; j < 3; j++) {
+      let temp = arr[i][j];
+      arr[i][j] = arr[j][i];
+      arr[j][i] = temp;
+      // console.log(`${i},${j} : ${arr[i][j]} , temp: ${temp} ; arr j i : ${arr[j][i]}`);
+    }
+  }
+  console.log(arr);
+
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n / 2; j++) {
+      let temp = arr[i][j];
+      arr[i][j] = arr[i][n - 1 - j];
+      arr[i][n - 1 - j] = temp;
+    }
+  }
+  console.log(arr);
+}
+
+// clockwiseRotate(smallArr);
+
+function combineArrayHorizontal(arr1, arr2) {
+  let newArr = [];
+  for (let i = 0; i < arr1.length; i++) {
+    let temp = [];
+    for (let j = 0; j < arr1[0].length; j++) {
+      temp.push(arr1[i][j]);
+      // console.log(temp);
+    }
+    for (let j = 0; j < arr2[0].length; j++) {
+      temp.push(arr2[i][j]);
+      // console.log(temp);
+    }
+    newArr.push(temp);
+
+  }
+  return newArr;
+}
+
+function combineArrayVertical(arr1, arr2) {
+  let newArr = [];
+  for (let i = 0; i < arr1.length; i++) {
+    let temp = [];
+    for (let j = 0; j < arr1[0].length; j++) {
+      temp.push(arr1[i][j]);
+      // console.log(temp);
+    }
+    newArr.push(temp);
+  }
+
+
+  for (let i = 0; i < arr2.length; i++) {
+    let temp = [];
+    for (let j = 0; j < arr2[0].length; j++) {
+      temp.push(arr2[i][j]);
+      // console.log(temp);
+    }
+    newArr.push(temp);
+  }
+  return newArr;
+}
+
+// console.log(combineArrayVertical(combineArrayHorizontal(smallArr1, smallArr2), combineArrayHorizontal(smallArr3, smallArr4)));
